703a2bcccf53ff525bbc77423b307ab24a88671a
Lilly Jinstrand <2537327+Kurinkitos@users.noreply.github.com>
2024-01-30 21:39:43 +0100

    Added some command line options to the c++ binary, can now be tested with hyperfine

--- ms-boost/queue.cpp
+++ ms-boost/queue.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <boost/atomic.hpp>
+#include <boost/program_options.hpp>
 
 boost::atomic_int producer_count(0);
 boost::atomic_int consumer_count(0);
@@ -10,8 +11,6 @@ boost::atomic_int consumer_count(0);
 boost::lockfree::queue<int> queue(128);
 
 const int iterations = 10000000;
-const int producer_thread_count = 4;
-const int consumer_thread_count = 4;
 
 void producer(void)
 {
@@ -37,9 +36,45 @@ void consumer(void)
 
 int main(int argc, char* argv[])
 {
+    namespace po = boost::program_options;
     using namespace std;
 
     boost::thread_group producer_threads, consumer_threads;
+
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("producers,p", po::value<int>(), "# producer threads")
+        ("consumers,c", po::value<int>(), "# consumer threads")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);    
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    int producer_thread_count = 4;
+    int consumer_thread_count = 4;
+
+    if (vm.count("producers")) {
+        cout << "Number of producers set to " 
+     << vm["producers"].as<int>() << ".\n";
+        producer_thread_count = vm["producers"].as<int>();
+    } else {
+        cout << "Number of producer threads not set, defaulting to 4\n";
+    }
+    if (vm.count("consumers")) {
+        cout << "Number of consumers set to " 
+     << vm["consumers"].as<int>() << ".\n";
+        consumer_thread_count = vm["consumers"].as<int>();
+    } else {
+        cout << "Number of consumer threads not set, defaulting to 4\n";
+    }
 
     for (int i = 0; i != producer_thread_count; ++i)
         producer_threads.create_thread(producer);
