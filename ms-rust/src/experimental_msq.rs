3655bbc40171e361eedec6efa0b8615b6c5800d4
mmackan <marcus_juhlin_@hotmail.se>
2024-01-31 15:46:57 +0100

    school session

--- ms-rust/src/experimental_msq.rs
+++ ms-rust/src/experimental_msq.rs
@@ -1,63 +1,77 @@
-use crossbeam::epoch::{self, Atomic, Owned};
-use std::sync::atomic::Ordering::{Relaxed};
+use crossbeam::{atomic::AtomicConsume, epoch::{self, Atomic, Owned}};
+use std::{alloc::{alloc, Layout}, ptr::null, sync::atomic::{AtomicPtr, AtomicUsize, Ordering::{Relaxed}}};
+
+use crate::tagged_ptr::TaggedPointer;
 
 struct Node {
     value: i32,
-    next: Atomic<Node>
+    next: TaggedPointer<Node>
 }
 
 impl Node {
     pub fn new(value: i32) -> Node {
+
         Node {
             value,
-            next: Atomic::null()
+            next: Default::default()
         }
     }
 }
 pub struct Queue {
-    head: Atomic<Node>,
-    tail: Atomic<Node> 
+    head: TaggedPointer<Node>,
+    tail: TaggedPointer<Node> 
 }
 
 impl Queue {
     pub fn new() -> Queue {
-        // SAFETY: dummy will never be modified, only read
-        let dummy = unsafe { 
-            Owned::new(Node::new(-1)).
-            into_shared(epoch::unprotected()) 
-        };
-        
-        Queue {
-            head: Atomic::from(dummy),
-            tail: Atomic::from(dummy)
+        let nullptr: AtomicPtr<Node> = Default::default();
+
+        // Calculate memory layout for node
+        let layout = Layout::new::<Node>();
+
+        // Allocate node to heap memory
+        unsafe {
+            let ptr = alloc(layout) as *mut Node;
+            *ptr = Node::new(-1);
+
+            Queue {
+                head: TaggedPointer::new(ptr, 0),
+                tail: TaggedPointer::new(ptr, 0)
+            }
         }
     }
 
     pub fn enqueue(&self, value: i32) {
-        
-        // Mark thread active in current epoch
-        let guard = epoch::pin();
-        
-        let node = Owned::new(Node::new(value)).into_shared(&guard);
+
+        // Calculate memory layout for node
+        let layout = Layout::new::<Node>();
+
+        // Allocate node to heap memory
+        let node_ptr = unsafe {
+            alloc(layout) as *mut Node
+        };
+        unsafe { *node_ptr = Node::new(value) };
 
         loop {                                                                    
             // Snapshots
-            let tail_ptr = self.tail.load(Relaxed, &guard);                        
-            let next_ptr = unsafe{ tail_ptr.deref() }.next.load(Relaxed, &guard); 
+            let tail_ptr = self.tail.load(Relaxed);                        
+            let next_ptr = unsafe{ *TaggedPointer::<Node>::remove_tag(tail_ptr)}.next.load(Relaxed); 
+
+            let next_tag = TaggedPointer::tag(next_ptr);
             
             // Tail snapshot still the queue's tail
-            if tail_ptr == 
-                self.tail.load(Relaxed, &guard) {         
+            if tail_ptr == self.tail.load(Relaxed) {         
                 
                 // Tail was pointing to the last node
-                if next_ptr.is_null() {                                                
+                if next_ptr == 0 {                                                
                     // Try link node at the end of linked list         
-                    match unsafe{ tail_ptr.deref() }.next.compare_exchange(   
+                    match unsafe{ *TaggedPointer::<Node>::remove_tag(tail_ptr) }.next.compare_exchange(   
                         next_ptr, 
-                        node.with_tag(next_ptr.tag() + 1), 
+                        node_ptr, TaggedPointer::<Node>::tag(next_ptr) + 1,
+                            //next_ptr.tag() + 1),
+                        // node.with_tag(next_ptr.tag() + 1), 
                         Relaxed, 
                         Relaxed, 
-                        &guard
                     ) {
                         Ok(_) => {
                             // Try update tail to inserted node
@@ -182,57 +196,57 @@ mod test {
         queue.enqueue(2);
         queue.enqueue(3);
 
-        // Normal removal
-        assert_eq!(queue.dequeue(), Some(1));
-        assert_eq!(queue.dequeue(), Some(2));
+        // // Normal removal
+        // assert_eq!(queue.dequeue(), Some(1));
+        // assert_eq!(queue.dequeue(), Some(2));
 
-        // Dequeue after dequeues
-        queue.enqueue(4);
-        queue.enqueue(5);
+        // // Dequeue after dequeues
+        // queue.enqueue(4);
+        // queue.enqueue(5);
 
-        // Normal removal to exhaustion
-        assert_eq!(queue.dequeue(), Some(3));
-        assert_eq!(queue.dequeue(), Some(4));
-        assert_eq!(queue.dequeue(), Some(5));
-        assert_eq!(queue.dequeue(), None);
+        // // Normal removal to exhaustion
+        // assert_eq!(queue.dequeue(), Some(3));
+        // assert_eq!(queue.dequeue(), Some(4));
+        // assert_eq!(queue.dequeue(), Some(5));
+        // assert_eq!(queue.dequeue(), None);
 
-        // Check the exhaustion case fixed the pointer right
-        queue.enqueue(6);
-        queue.enqueue(7);
+        // // Check the exhaustion case fixed the pointer right
+        // queue.enqueue(6);
+        // queue.enqueue(7);
 
-        // Normal removal again
-        assert_eq!(queue.dequeue(), Some(6));
-        assert_eq!(queue.dequeue(), Some(7));
-        assert_eq!(queue.dequeue(), None);
+        // // Normal removal again
+        // assert_eq!(queue.dequeue(), Some(6));
+        // assert_eq!(queue.dequeue(), Some(7));
+        // assert_eq!(queue.dequeue(), None);
     }
 
-    #[test]
-    fn basic_concurrent() {
-        let queue = Arc::new(Queue::new());
-        let mut handles = vec![];
+    // #[test]
+    // fn basic_concurrent() {
+    //     let queue = Arc::new(Queue::new());
+    //     let mut handles = vec![];
 
-        let n = 10;
+    //     let n = 10;
 
-        for i in 0..n {
-            let queue = Arc::clone(&queue);
-            let handle = thread::spawn(move || {
-                queue.enqueue(i)
-            });
-        handles.push(handle);
-        }
+    //     for i in 0..n {
+    //         let queue = Arc::clone(&queue);
+    //         let handle = thread::spawn(move || {
+    //             queue.enqueue(i)
+    //         });
+    //     handles.push(handle);
+    //     }
 
-        for handle in handles {
-            handle.join().unwrap();
-        }
+    //     for handle in handles {
+    //         handle.join().unwrap();
+    //     }
         
-        let mut dequeue_sum = 0;
-        while let Some(value) = queue.dequeue() {
-            dequeue_sum += value;
-        }
+    //     let mut dequeue_sum = 0;
+    //     while let Some(value) = queue.dequeue() {
+    //         dequeue_sum += value;
+    //     }
 
-        // Sum of first n natural numbers (0 to n-1)
-        let expected_sum = n * (n - 1) / 2;
+    //     // Sum of first n natural numbers (0 to n-1)
+    //     let expected_sum = n * (n - 1) / 2;
 
-        assert_eq!(expected_sum, dequeue_sum, "Sums do not match!");
-    }
+    //     assert_eq!(expected_sum, dequeue_sum, "Sums do not match!");
+    // }
 }
