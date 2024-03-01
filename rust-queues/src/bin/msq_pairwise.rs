a34ad21339d0adb4aa8de5d39608c834835eb3dc
Lilly Jinstrand <2537327+Kurinkitos@users.noreply.github.com>
2024-03-01 12:16:23 +0100

    Changed the MSQ pairwise benchmark to use the new framework

---
+++ rust-queues/src/bin/msq_pairwise.rs
@@ -0,0 +1,41 @@
+use std::env;
+
+use rust_queues::ms_queue::msq_hazp::MSQueue;
+use rust_queues::shared_queue::SharedQueue;
+
+use rust_queues::pairwise_benchmark;
+
+/// Default exponent for # operations
+const LOGN_OPS: u32 = 7;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <threads> [exponent_base_ten]", args[0]);
+        std::process::exit(1);
+    }
+
+    let threads: u32 = args[1].parse().expect("Number of threads must be a positive integer");
+    if threads == 0 {
+        eprintln!("Number of threads cannot be 0.");
+        std::process::exit(1);
+    }
+
+    let logn: u32 = if args.len() > 2 {
+        args[2].parse().expect("Exponent must be positive integer")
+    } else {
+        LOGN_OPS
+    };
+
+    println!("===========================================");
+    println!("  Benchmark: {}", args[0]);
+    println!("  Threads: {}", threads);
+    println!("  Operations: 10^{}", logn);
+
+    let queue = MSQueue::new();
+
+    pairwise_benchmark::benchmark(threads, logn, queue);
+
+    println!("  Finished");
+}
