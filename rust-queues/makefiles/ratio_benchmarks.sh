116e5b024feb5166b0dbf554085d8eca6674b8cc
mmackan <marcus_juhlin_@hotmail.se>
2024-04-16 13:30:06 +0200

    Move script code to seperate script

--- rust-queues/makefiles/ratio_benchmarks.sh
+++ rust-queues/makefiles/ratio_benchmarks.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# Directory of the script
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Temporary directory within $dir to store intermediate hyperfine results
-temp_dir=`mktemp -d -p "$dir"`
-
-# Check if temp dir was created
-if [[ ! "$temp_dir" || ! -d "$temp_dir" ]]; then
-  echo "Could not create temp dir"
-  exit 1
-fi
-
-# Deletes the temp directory
-function cleanup {      
-  rm -rf "$temp_dir"
-  echo "Deleted temp working directory $temp_dir"
-}
-
-# Register the cleanup function to be called on the EXIT signal
-trap cleanup EXIT
+# Create temporary directory to store intermediate hyperfine results
+source "${SCRIPT_DIR}/setup_temp_dir.sh"
 
 # Multipliers
 producer_multiplier="$1" 
