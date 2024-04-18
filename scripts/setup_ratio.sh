b6f6d12fded8853cd9a347b53104c0b87076d394
mmackan <marcus_juhlin_@hotmail.se>
2024-04-18 19:51:23 +0200

    Add memory usage benchmark, using perf

---
+++ scripts/setup_ratio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo HEEEEEEEEEY
+FACTOR=$1
+RATIO=$2
+
+max_threads=$(($(nproc) / $FACTOR))
+
+# Extract the multipliers from the ratio
+IFS=':' read producer_multiplier consumer_multiplier <<< "$RATIO"
+
+# Calculate maximum amount of producers & consumers for the ratio
+min_threads=$((producer_multiplier + consumer_multiplier))
+base=$(($max_threads / $min_threads))
+
+producers=$(($base * producer_multiplier))
+consumers=$(($base * consumer_multiplier))
+
+export PROD=$producers
+export CONS=$consumers
+
+
+
