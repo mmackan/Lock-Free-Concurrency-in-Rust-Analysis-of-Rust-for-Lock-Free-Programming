87594b5efdc6d6181ae89c6f3ae183ec4559c9ba
Kurinkitos <2537327+Kurinkitos@users.noreply.github.com>
2024-04-22 10:54:43 +0200

    Switched from linear to exponential backoff

--- c++-reference/src/lprq_mpmc.cpp
+++ c++-reference/src/lprq_mpmc.cpp
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]){
             // Thread rng
             auto engine = std::mt19937(std::random_device{}());
             auto distribution = std::uniform_real_distribution<float>(0.0,1.0);
-            int backoff = 0;
+            int backoff = 1;
 
             // Cpu affinity
             cpu_set_t cpuset;
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]){
                     if (done.load() == true) {
                         break;
                     }
-                    backoff++;
+                    backoff = backoff * 2;
                     for(int j = 0; j < backoff; j++) {
                         delay_exec();
                     }
